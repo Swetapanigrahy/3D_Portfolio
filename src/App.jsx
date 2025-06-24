39cdae1515453f6ca73be961b335e77a9e7b58f6
Swetapanigrahy <swetaranipanigrahy13@gmail.com>
2025-06-24 18:44:11 +0530

    Navbar,worksection, experiencesection, featurecard part done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 // App.jsx
-import React from 'react';
-import Hero from './sections/Hero.jsx'
-
+import React from "react";
+import Hero from "./sections/Hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/Navbar.jsx";
+import LogoShowcase from "./sections/LogoShowcase.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+import Experience from "./sections/Experience.jsx";
 
 function App() {
- 
-
   return (
     <div>
-       
-      <Hero/> 
-     
+      <NavBar />
+      <Hero />
+      <ShowcaseSection />
+<LogoShowcase/> 
+   <FeatureCards/> 
+   <Experience/> 
     </div>
   );
 }
-
 
 export default App;
