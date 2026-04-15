cfa25196060572c0a9f0387b3113f2bd270b0af3
Sweta panigrahy <152600276+Swetapanigrahy@users.noreply.github.com>
2026-04-15 17:44:53 +0530

    Refactor App component and remove smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-// App.jsx
-import React, { useEffect } from "react";
+import React from "react";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/Navbar.jsx";
 import Experience from "./sections/Experience.jsx";
 import Skills from "./components/Models/Skills.jsx";
 import Contact from "./sections/Contact.jsx";
-import { initSmoothScrolling } from "./utils/smoothScroll";
 
 function App() {
-  // Initialize smooth scrolling when the app loads
-  useEffect(() => {
-    initSmoothScrolling();
-  }, []);
   return (
-    <div className="relative bg-black">
+    <div className="bg-black">
       <NavBar />
       <main>
         <Hero />
-        <div className="relative z-10">
-          <ShowcaseSection />
-          <Skills />
-          <Experience />
-          <Contact />
-        </div>
+        <ShowcaseSection />
+        <Skills />
+        <Experience />
+        <Contact />
       </main>
     </div>
   );
