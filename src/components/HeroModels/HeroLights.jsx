fc8df31506e6b5a61e45397c347f4ddf346da60b
Sweta panigrahy <152600276+Swetapanigrahy@users.noreply.github.com>
2026-04-15 17:34:50 +0530

    Refactor HeroLights to improve lighting setup

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -1,42 +1,56 @@
 import * as THREE from "three";
+import { useRef } from "react";
 
-const HeroLights = () => (
-  <>
-    {/* lamp's light */}
-    <spotLight
-      position={[2, 5, 6]}
-      angle={0.15}
-      penumbra={0.2}
-      intensity={100}
-      color="white"
-    />
-    {/* bluish overhead lamp */}
-    <spotLight
-      position={[4, 5, 4]}
-      angle={0.3}
-      penumbra={0.5}
-      intensity={40}
-      color="#4cc9f0"
-    />
-    {/* purplish side fill */}
-    <spotLight
-      position={[-3, 5, 5]}
-      angle={0.4}
-      penumbra={1}
-      intensity={60}
-      color="#9d4edd"
-    />
-    {/* area light for soft moody fill */}
-    <primitive
-      object={new THREE.RectAreaLight("#a259ff", 8, 3, 2)}
-      position={[1, 3, 4]}
-      rotation={[-Math.PI / 4, Math.PI / 4, 0]}
-      intensity={15}
-    />
-    {/* subtle point light for atmospheric tone */}
-    <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
-    <pointLight position={[1, 2, -2]} intensity={10} color="#0d00a4" />
-  </>
-);
+const HeroLights = () => {
+  const mainLight = useRef();
+
+  return (
+    <>
+
+      <directionalLight
+        ref={mainLight}
+        position={[5, 5, 5]}
+        intensity={2}
+      />
+
+      {/* lamp light */}
+      <spotLight
+        position={[2, 5, 6]}
+        angle={0.15}
+        penumbra={0.2}
+        intensity={40}
+      />
+
+      {/* blue light */}
+      <spotLight
+        position={[4, 5, 4]}
+        angle={0.3}
+        penumbra={0.5}
+        intensity={20}
+        color="#4cc9f0"
+      />
+
+      {/* purple fill */}
+      <spotLight
+        position={[-3, 5, 5]}
+        angle={0.4}
+        penumbra={1}
+        intensity={30}
+        color="#9d4edd"
+      />
+
+      {/* area light */}
+      <primitive
+        object={new THREE.RectAreaLight("#a259ff", 5, 3, 2)}
+        position={[1, 3, 4]}
+        rotation={[-Math.PI / 4, Math.PI / 4, 0]}
+      />
+
+      {/* soft atmosphere */}
+      <pointLight position={[0, 1, 0]} intensity={3} color="#7209b7" />
+      <pointLight position={[1, 2, -2]} intensity={3} color="#0d00a4" />
+    </>
+  );
+};
 
 export default HeroLights;
