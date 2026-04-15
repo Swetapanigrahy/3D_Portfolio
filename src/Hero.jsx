658619787a8e84f6428ca17bd73bcd5d24b1433c
Sweta panigrahy <152600276+Swetapanigrahy@users.noreply.github.com>
2026-04-15 18:28:43 +0530

    Refactor hero-3d-layout div structure

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
 
         {/* RIGHT: 3D Model or Visual */}
         <figure>
-          <div className="hero-3d-layout relative w-full h-[60vh] md:h-[80vh]">
+          <div className="hero-3d-layout">
             <HeroExperience />
           </div>
         </figure>
